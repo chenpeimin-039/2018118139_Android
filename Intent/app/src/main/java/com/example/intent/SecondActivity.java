f2288a5b8b2b13939dacb2c44219230fd31c657d
chenpeimin-039 <2496935851@qq.com>
2020-11-15 17:32:20 +0800

    实验四（Intent应用）

---
+++ Intent/app/src/main/java/com/example/intent/SecondActivity.java
@@ -0,0 +1,46 @@
+package com.example.intent;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class SecondActivity extends BaseActivity {
+
+    //传递数据并存储到Intent中
+    public static void actionStart(Context context, String data1, String data2) {
+        Intent intent = new Intent(context, SecondActivity.class);
+        intent.putExtra("param1", data1);
+        intent.putExtra("param2", data2);
+        context.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("SecondActivity", "Task id is " + getTaskId());
+        setContentView(R.layout.second_layout);
+        Button button2 = (Button) findViewById(R.id.button_2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SecondActivity.this, ThirdActivity.class);//使用显式Intent
+                startActivity(intent);
+            }
+        });
+    }
+
+    //确保传输数据时，用户不是按下按钮而是按下Back键时，还是能够返回数据
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent();
+        intent.putExtra("data_return", "Hello FirstActivity");
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
+}
+
