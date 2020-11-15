8ba6891b9481b870aa02327949b3e0c494cb31ff
chenpeimin-039 <2496935851@qq.com>
2020-11-15 16:54:23 +0800

    实验七（自定义广播的使用）

---
+++ BroadcastTest/app/src/main/java/com/example/broadcasttest/MyBroadcastReceiver.java
@@ -0,0 +1,14 @@
+package com.example.broadcasttest;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+//广播接收器
+public class MyBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "received in MyBroadcastReceiver", Toast.LENGTH_SHORT).show();
+    }
+}
